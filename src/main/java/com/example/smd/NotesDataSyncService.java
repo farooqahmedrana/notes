9b76b966840e587c024a5a8ecb51f87c80096992
Farooq Ahmed <farooqahmedrana@gmail.com>
2016-09-04 21:20:00 +0500

    data sync service connecting to web

--- src/main/java/com/example/smd/NotesDataSyncService.java
+++ src/main/java/com/example/smd/NotesDataSyncService.java
@@ -4,6 +4,14 @@ import android.app.*;
 import android.os.*;
 import android.content.*;
 import android.widget.Toast;
+import android.util.Log;
+import java.net.*;
+import java.io.*;
+import android.content.*;
+import android.provider.*;
+import android.database.*;
+import android.net.Uri;
+
 
 public class NotesDataSyncService extends Service {
 
@@ -17,6 +25,14 @@ public class NotesDataSyncService extends Service {
 
    public int onStartCommand(Intent intent,int flags,int startId){
       Toast.makeText(this,"Service starting",Toast.LENGTH_SHORT).show();
+
+      Thread thread = new Thread(new Runnable(){
+         public void run(){
+            post();
+         }
+      });
+
+      thread.start();
       return START_NOT_STICKY;
    }
 
@@ -40,26 +56,90 @@ public class NotesDataSyncService extends Service {
       public void handleMessage(Message msg){
          switch(msg.what){
             case MSG_TEST:
-//               Toast.makeText(getApplicationContext(),getStatus(),Toast.LENGTH_SHORT).show();
-                 Message reply = Message.obtain(null,1);
-                 Bundle bundle = new Bundle();
-                 bundle.putString("result",getStatus());
-                 reply.setData(bundle);
-                 Messenger receiver = msg.replyTo;
-                 try {
-                    receiver.send(reply);
-                 } catch (RemoteException ex) { ex.printStackTrace();}
-
+                 sendReply(msg,msg.replyTo,getStatus());
             default:
                super.handleMessage(msg);
          }         
       }
    }
 
+/*   public void handleDownloadMessage(final Message msg,final Messenger messenger){
+      Thread thread = new Thread(new Runnable(){
+         public void run(){
+            sendReply(msg,messenger,postStatus());
+         }
+      });
 
+      thread.start();
+   }
+*/
+   public void sendReply(final Message msg,final Messenger receiver,String content){
+
+      Message reply = Message.obtain(null,1);
+      Bundle bundle = new Bundle();
+      bundle.putString("result",content);
+      reply.setData(bundle);
+
+      try {
+         receiver.send(reply);
+      } catch (RemoteException ex) { ex.printStackTrace();}
+
+   }
 
    public String getStatus(){
       return "synchronization in progress";
    }
 
+   public String post(){
+
+      StringBuilder content = new StringBuilder();
+
+      try{
+
+          URL url = new URL("http://10.0.2.2/notes/upload.php");
+          HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+          connection.setReadTimeout(10000);
+          connection.setConnectTimeout(15000);
+          connection.setRequestMethod("POST");
+          connection.setRequestProperty("Content-type","text/xml");
+          connection.setDoOutput(true);
+          connection.connect();
+
+          BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+          writer.write(getNotesContent());
+          writer.flush();
+
+          BufferedReader reader = new BufferedReader( new InputStreamReader( connection.getInputStream() ) );
+          
+          String line = "";
+          while( (line = reader.readLine()) != null ){
+             content.append(line);
+          }
+
+          connection.disconnect();
+
+      } catch(Exception ex) {
+           ex.printStackTrace();
+      }
+
+      return content.toString();
+   }
+
+   public String getNotesContent(){
+
+      String uri = "content://com.example.smd.notesprovider/notes";
+      Cursor cursor = getContentResolver().query(Uri.parse(uri),null,null,null,null);
+              
+      StringBuilder result = new StringBuilder();
+      while (cursor != null && cursor.moveToNext()){
+         String title = cursor.getString(cursor.getColumnIndex("Title"));
+         String content = cursor.getString(cursor.getColumnIndex("Content"));
+         result.append ("<note title='" + title + "' content='" + content + "'/>");
+      }
+
+      return "<notes>" + result.toString() + "</notes>";
+   }
+
+
 }
+
